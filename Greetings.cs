ceeaadfa3b3f879c09cc2ea1048b47ee4cc87f62
Atom1cYT <146125539+Atom1cYT@users.noreply.github.com>
2024-04-10 09:35:44 -0500

    Initial commit

---
+++ Greetings.cs
@@ -0,0 +1,23 @@
+﻿    using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bruner_Zach_Classes
+{
+    internal class Greetings
+    {
+
+        public void Welcome()
+            // Says Hello username welcome
+        {
+            Console.WriteLine("Hello Travler, welcome!");
+        }
+
+        public void Hello(string name)
+            // Passes through saying Thank you for joining us
+        {
+            Console.WriteLine($"{name}, thank you for joining us today!");
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿    using System;
+
+namespace Bruner_Zach_Classes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Creates a new greeting object called greetings
+            Greetings greetings = new Greetings();
+            // Calls the welcome method on the greetings
+            greetings.Welcome();
+            
+            // Puts What is your name squire into the command prompt
+            Console.WriteLine("What is your name squire?");
+            // gets input from user
+            string userName = Console.ReadLine();
+            // Calls the Hello users method and passes the UserName
+            greetings.Hello(userName);
+        }
+    }
+}
